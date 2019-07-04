15c56d7506898ba256ecbf5fca41ba7dc684c46a
yujunjiee <1711425052@qq.com>
2019-07-04 19:33:18 +0800

    第二天测试代码

---
+++ public/assets/js/categories.js
@@ -0,0 +1,75 @@
+//添加分类
+$('#addCategory').on('submit',function(){
+  console.log($(this).serialize());
+  $.ajax({
+    type:'post',//get或post
+    url:'/categories',//请求的地址
+    data:$(this).serialize(),//如果不需要传，则注释掉 请求的参数，a=1&b=2或{a:1,b:2}或者jq中的serialize方法，或者formData收集
+    dataType:'json',
+    success:function(result){//成功的回调函数
+      console.log(result)
+      location.reload();
+    }
+  })
+  return false;
+})
+
+//展示分类列表数据
+$.ajax({
+  type:'get',//get或post
+  url:'/categories',//请求的地址
+  success:function(result){//成功的回调函数
+    // console.log(result)
+    var html = template('categoryListTpl',{data:result});
+    $('#categoryList').html(html);
+  }
+})
+
+//当点击编辑按钮的时候，让当前这一行的内容展示在左侧的表单上面
+$('#categoryList').on('click','.edit',function(){
+  var id = $(this).attr('data-id');
+  // console.log(id);
+  $.ajax({
+    type:'get',//get或post
+    url:'/categories/'+id,//请求的地址
+    data:{},//如果不需要传，则注释掉 请求的参数，a=1&b=2或{a:1,b:2}或者jq中的serialize方法，或者formData收集
+    success:function(result){//成功的回调函数
+      console.log(result)
+      var html = template('modifyFormTpl',result);
+      $('#formBox').html(html);
+    }
+  })
+})
+
+//当提交修改表单的时候提交ajax
+$('#formBox').on('submit','#addCategory',function(){
+  //收集表单数据ajax
+  var formData = $(this).serialize();
+  var id = $(this).attr('data-id');
+  $.ajax({
+    type:'put',//get或post
+    url:'/categories/'+id,//请求的地址
+    data:formData,
+    success:function(result){//成功的回调函数
+      // console.log(result)
+      location.reload();
+    }
+  })
+  return false;
+})
+
+//删除功能
+$('#categoryList').on('click','.delete',function(){
+ if(confirm('确定要删除吗？')){
+  var id = $(this).attr('data-id');
+  $.ajax({
+    type:'delete',//get或post
+    url:'/categories/'+id,//请求的地址
+    data:{},//如果不需要传，则注释掉 请求的参数，a=1&b=2或{a:1,b:2}或者jq中的serialize方法，或者formData收集
+    success:function(result){//成功的回调函数
+      console.log(result)
+      location.reload();
+    }
+  })
+ }
+})
---
+++ public/assets/js/password-reset.js
@@ -0,0 +1,16 @@
+//当修改密码表单发生提交行为的时候
+$('#modifyForm').on('submit', function() {
+  var formData = $(this).serialize();
+  // console.log(formData)
+  //调用接口 实现密码修改功能
+  $.ajax({
+    url: '/users/password',
+    type: 'put',
+    data: formData,
+    success: function() {
+      location.href = '/admin/login.html';
+    }
+  })
+  //阻止表单默认提交的行为
+  return false;
+})
